95d99a0d8ea2e8f327febe8310ce5eb158683f02
Alechko <Alechko@ALECHKO-PC>
2017-01-02 21:55:02 +0200

    Changes made

--- Shared_Project1/Program.cs
+++ Shared_Project1/Program.cs
@@ -14,7 +14,7 @@ namespace Shared_Project1
             string b = "fuck you alex lol";
             Console.WriteLine(a);
             Console.WriteLine(b);
-            Console.ReadKey();
+            
         }
     }
 }
